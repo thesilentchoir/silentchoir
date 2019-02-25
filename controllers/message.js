27d39dd9a8cd37a9a7909a87f3311d13829d07d2
Madeline OLeary <madeline.w.oleary@gmail.com>
2019-02-25 10:56:42 +0100

    simplifying message functionality just to get socket io up and running properly

--- controllers/message.js
+++ controllers/message.js
@@ -1,91 +1,21 @@
 const Message = require('../models/Message');
 const Room = require('../models/Room');
 
-exports.getNewMessage = (req, res) => {
-  Room.findById(req.params.roomId, (err, room) => {
-    let participantsObject = room.participants;
-    let participantStringsArray = participantsObject.map(id => JSON.stringify(id));
-
-    let idCheck = JSON.stringify(req.user.id);
-
-    if (!participantStringsArray.includes(idCheck)) {
-      req.flash('errors', { msg: 'Oops! Please pick a chatroom of which you are a member.' });
-      return res.redirect('/account');
-    }
-
-    res.render('messages/new');
-  })
+exports.getMessages = (req, res) => {
+  res.render('messages');
 }
 
-exports.listMessages = (req, res) => {
-  Room.findById(req.params.roomId, (err, room) => {
-    let participantsObject = room.participants;
-    let participantStringsArray = participantsObject.map(id => JSON.stringify(id));
-
-    let idCheck = JSON.stringify(req.user.id);
-
-    console.log("HERE")
-
-    if (!participantStringsArray.includes(idCheck)) {
-      req.flash('errors', { msg: 'Oops! Please pick a chatroom of which you are a member.' });
-      return res.redirect('/account');
-    }
-
-    res.render('messages', { messages: room.messages });
-  })
-}
-
-exports.createMessage = (req, res) => {
-  req.assert('text', 'Message cannot be blank').notEmpty();
-
-  const errors = req.validationErrors();
-
-  if (errors) {
-    req.flash('errors', errors);
-    return res.redirect('rooms/:roomId/messages');
-  }
-
-  let message = new Message(
+exports.postMessages = (req, res) => {
+  var message = new Message(
     {
-      text: req.body.text,
-      owner: req.user,
-      room: req.params.roomId
+      text: req.body.text
     }
   );
-
-  message.save(function (err) {
+  message.save((err) => {
     if (err) {
-      return next(err);
+      sendStatus(500);
     }
-
-    Room.findById(req.params.roomId, (err, room) => {
-      room.messages.push(message);
-      room.save();
-    })
-
-    Room.findById(req.params.roomId, (err, room) => {
-      res.render('messages', { messages: room.messages });
-    })
+    // io.emit('message', req.body);
+    res.sendStatus(200);
   })
-}
-
-exports.deleteMessage = (req, res) => {
-  Message.findByIdAndRemove(req.params.messageId)
-  .then(message => {
-    if (!message) {
-      return res.status(404).send({
-        message: "Message not found!"
-      });
-    }
-    res.send({message: "Message deleted successfully."});
-  }).catch(err => {
-    if(err.kind === 'ObjectId' || err.name === 'NotFound') {
-      return res.status(404).send({
-        message: "Message not found: " + req.params.messageId
-      });
-    }
-    return res.status(500).send({
-      message: "Could not delete message: " + req.params.messageId
-    });
-  });
 }
--- models/Message.js
+++ models/Message.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const messageSchema = new mongoose.Schema({
-    room: {
-        type: ObjectId,
-        ref: 'Room'
-        // required: true
-    },
-    owner: {
-        type: ObjectId,
-        ref: 'User'
-        // required: true
-    },
+    // room: {
+    //     type: ObjectId,
+    //     ref: 'Room'
+    //     // required: true
+    // },
+    // owner: {
+    //     type: ObjectId,
+    //     ref: 'User'
+    //     // required: true
+    // },
     text: {
         type: String
         // required: true
