3502bdf4db55fa02aafa03e9e27ed18c98128a45
Madeline OLeary <madeline.w.oleary@gmail.com>
2019-03-25 11:34:51 +0100

    socket client code for messages -- move to its own independent js file -- to-do

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,23 @@
 /* eslint-env jquery, browser */
+
 $(document).ready(() => {
 
   // Place JavaScript code here...
-  var socket = io.connect('http://localhost:8080')
-  var message = $("#message");
-  var username = $("#username");
-  var send_message = $("#send_message");
-  var send_username = $("#send_username");
-  var chatroom = $("#chatroom")
+  const socket = io.connect('http://localhost:8080')
+  let message = $("#message");
+  let username = $("#username");
+  let room_id = $("#roomId")
+  let send_message = $("#send_message");
+  let chatroom = $(".chat-row")
 
   send_message.click(function() {
-    socket.emit('new_message', { message : message.val() });
+    console.log(message.val());
+    console.log(username.val());
+    socket.emit('new_message', { message : message.val(), username: username.val(), roomId: room_id.val() });
   })
 
   socket.on("new_message", (data) => {
     console.log(data);
-    chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+    chatroom.append("<li class='message'>" + data.username + ": " + data.message + "<li>");
   })
-
-  send_username.click(function() {
-    console.log(username.val())
-    socket.emit('change_username', { username : username.val() });
-  })
-
 });
