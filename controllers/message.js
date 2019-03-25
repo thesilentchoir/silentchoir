4992021d5bc8bf332d39d366a9fc49a44b498674
Madeline OLeary <madeline.w.oleary@gmail.com>
2019-03-25 08:27:33 +0100

    adding complexity back to messages route for security

--- controllers/message.js
+++ controllers/message.js
@@ -1,16 +1,27 @@
 const Message = require('../models/Message');
 const Room = require('../models/Room');
+// const express = require('express');
+// const app = express();
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
 
 exports.getMessages = (req, res) => {
-  res.render('messages');
-}
+  let currentUser = req.user.username;
 
-exports.postMessages = (req, res) => {
-  var message = new Message(req.body);
-  message.save((err) =>{
-    if(err)
-      sendStatus(500);
-    io.emit('message', req.body);
-    res.sendStatus(200);
-  })
+  Room.findById({ _id: req.params.roomId }, (err, room) => {
+    if (err) { return next(err); }
+
+    // check participants
+    let participantsObject = room.participants;
+    let participantStringsArray = participantsObject.map(id => JSON.stringify(id));
+
+    let currentUserId = JSON.stringify(req.user.id);
+
+    if (participantStringsArray.includes(currentUserId)) {
+      req.flash('success', { msg: 'Welcome!' });
+      return res.render('messages', { messages: room.messages, username: currentUser });
+    } else {
+      return res.render('error-forbidden')
+    }
+  });
 }
