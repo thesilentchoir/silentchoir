08ad243d318c5ae16f61909b66a53c862ae79703
Madeline OLeary <madeline.w.oleary@gmail.com>
2019-03-25 11:32:23 +0100

    tweaking message model

--- models/Message.js
+++ models/Message.js
@@ -2,17 +2,12 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const messageSchema = new mongoose.Schema({
-    // room: {
-    //     type: ObjectId,
-    //     ref: 'Room'
-    //     // required: true
-    // },
-    // owner: {
-    //     type: ObjectId,
-    //     ref: 'User'
-    //     // required: true
-    // },
-    name: {
+    room: {
+        type: ObjectId,
+        ref: 'Room'
+        // required: true
+    },
+    username: {
         type: String
         // required: true
     },
